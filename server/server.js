10b2372e70de5a5339d0e5d113b3b928cfd871b6
creaticodedev-ops <creaticodedev@gmail.com>
2026-08-05 14:15:41 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ await connectDB();
 
 const allowedOrigins = process.env.CLIENT_URL
   ? process.env.CLIENT_URL.split(",").map((o) => o.trim()).filter(Boolean)
-  : ["https://americonfort.vercel.app","http://localhost:5173", "http://localhost:3000"];
+  : ["https://americonfort.com","https://www.americonfort.com","http://localhost:5173", "http://localhost:3000"];
 
 app.use(
   cors({
